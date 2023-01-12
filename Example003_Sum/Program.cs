d4fb8cc2dc8a3e30ba20f6257e740e3b0277cb2e
Galiya <safaga71@mail.ru>
2023-01-12 14:24:11 +0300

    Random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 30;
-int numberВ = 5;
-int result = numberA + numberВ;
-Console.WriteLine(result );
+﻿int numberA = new Random().Next(1,10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);
